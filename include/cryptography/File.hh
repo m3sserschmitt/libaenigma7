4b70cc1b43710646aac053abd31bacc5934dd09a
Romulus Ruja <romulus-emanuel.ruja@tutanota.com>
2022-12-29 14:49:57 +0200

    added File.cc source

--- include/cryptography/File.hh
+++ include/cryptography/File.hh
@@ -1,6 +1,8 @@
 #ifndef FILE_HH
 #define FILE_HH
 
+#include "Types.hh"
+
 #include <fstream>
 #include <string>
 #include <cstring>
@@ -8,32 +10,7 @@
 class File
 {
 public:
-    static const char *readFile(const char *path, Size &len)
-    {
-        std::string line;
-        std::string content;
-        std::ifstream in(path, std::ifstream::in);
-        
-        len = 0;
-
-        if(not in.is_open())
-        {
-            return nullptr;
-        }
-
-        while(getline(in, line))
-        {
-            content += line;
-        }
-
-        len = content.size();
-
-        char *data = new char[len + 1];
-        strcpy(data, content.c_str());
-        data[len] = 0;
-
-        return data;
-    }
+    static const char *readFile(const char *path, Size &len);
 };
 
 #endif
